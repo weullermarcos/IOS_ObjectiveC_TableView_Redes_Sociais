6875954e336f1288ad4b4e339b8a99077f199f48
Weuller Marcos <weullermarcos@MacBook-Pro-de-Weuller.local>
2016-11-06 15:07:02 -0200

    Facebook

---
+++ ExerciciosXib/ExerciciosXib/FacebookTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FacebookTableViewCell.h
+//  ExerciciosXib
+//
+//  Created by Weuller Marcos on 06/11/16.
+//  Copyright © 2016 Weuller Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookTableViewCell : UITableViewCell
+
+@end
---
+++ ExerciciosXib/ExerciciosXib/FacebookTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  FacebookTableViewCell.m
+//  ExerciciosXib
+//
+//  Created by Weuller Marcos on 06/11/16.
+//  Copyright © 2016 Weuller Marcos. All rights reserved.
+//
+
+#import "FacebookTableViewCell.h"
+
+@implementation FacebookTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
